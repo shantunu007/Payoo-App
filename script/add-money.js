15a4ef646f51fecb694a86d9266714dca37c8327
shantunu007 <chakmashantunu.web@gmail.com>
2025-02-25 11:22:51 +0600

    completed the balance js

---
+++ script/add-money.js
@@ -0,0 +1,17 @@
+
+document.getElementById("btn-add-money").addEventListener("click", function(event){
+    event.preventDefault();
+    const amount = document.getElementById("amount").value;
+    const convertedAmount = parseFloat(amount);
+    const pinNumber = document.getElementById("pin-number").value;
+    const convertedPin = parseInt(pinNumber);
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const convertedBalance = parseFloat(mainBalance);
+    if(convertedPin === 1234){
+        const sum = convertedBalance + convertedAmount;
+        document.getElementById("main-balance").innerText = sum;
+    }
+    else{
+        console.log("wrong pin number")
+    }
+})
